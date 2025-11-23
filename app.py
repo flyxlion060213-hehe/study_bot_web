49e6c70d5dd1bcb22585d5fabe78b5f6f493f3c2
Nguyen Thanh Cong <flyxlion060213@gmail.com>
2025-11-23 14:17:41 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from google import genai
 import os, json
 
 # ==== API KEY ====
-GEMINI_API_KEY = "AIzaSyDooDrXQaWCIhkHJwyno8ecxSB2ShHWQbM"
+GEMINI_API_KEY = "AIzaSyA4jR4s2gfzoiQ2mgeY0GkXPYQkG9JhNh8"
 client = genai.Client(api_key=GEMINI_API_KEY)
 
 app = Flask(__name__)
